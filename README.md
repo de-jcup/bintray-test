a49ca94b526743b04fa06d36c7164772e506454e
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2016-12-30 23:55:52 +0100

    Added parts of egradle 0.7.1 as test base

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bintray-test
 A simple repository to test bintray behaviours
+
+Current state:
+
+- Content of EGradle Version 0.7.1 contained
+
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BINTRAY_USER=$1
+BINTRAY_API_KEY=$2
+
+function show_help_and_exit() {
+	echo "Usage:"
+	echo " deploy.sh bintrayUser apiKey"
+	exit 1
+}
+
+if [ -z "$BINTRAY_USER" ]; then
+	echo "bintray user not set"
+	show_help_and_exit;
+fi
+if [ -z "$BINTRAY_API_KEY" ]; then
+	echo "bintray api key not set"
+	show_help_and_exit;
+fi
+# use bintray upload script from vogella:
+# https://github.com/vogellacompany/bintray-publish-p2-updatesite (de-jcup fork)
+./../bintray-publish-p2-updatesite/pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup bintray-test update-site current ./test-updatesite/
